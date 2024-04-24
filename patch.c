51028e2f62c653565d30a5c5d9eb79c7af9fe48e
lucia <llcsaturnaliam@gmail.com>
2024-04-23 22:48:23 -0500

    implemented basic peb patching

--- patch.c
+++ patch.c
@@ -1,6 +1,13 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <winternl.h>
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD_PTR ul_reason_for_call, LPVOID lpReserved) {
-  MessageBoxA(NULL, "test?", "does this work <3", MB_OK);
+  if (ul_reason_for_call == DLL_PROCESS_ATTACH) {  
+    // i should probably make this detect whether its attached to a 32bit or 64bit process but i couldnt care less loll
+    PPEB pPeb = (PPEB)__readgsqword(0x60);
+    pPeb->BeingDebugged = 0;
+  }
+
+  return TRUE;
 }
